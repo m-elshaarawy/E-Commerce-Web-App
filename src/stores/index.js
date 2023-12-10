93fb57740382bc2ff71ab66930cecf298b6d7a60
mohamed_elshaarawy <m.elshaarawy96@yahoo.com>
2023-12-10 13:47:37 +0200

    installing pakages

---
+++ src/stores/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex'
+// import * from './modules'
+export default createStore({
+    strict: true,
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {
+      
+    },
+  })
